1d17fb1221b80f7bb7c616d1c6d93883f801a539
Abhinav <abhinavprajapati@gmail.com>
2024-05-03 16:08:01 +0530

    Add adb.py file for taking device screenshots

---
+++ adb.py
@@ -0,0 +1,9 @@
+from ppadb.client import Client as AdbClient
+client = AdbClient()
+device = client.device("79dadda0")
+
+def get_screenshrot():
+    result = device.screencap()
+    with open("screen.png", "wb") as fp:
+        fp.write(result)
+
